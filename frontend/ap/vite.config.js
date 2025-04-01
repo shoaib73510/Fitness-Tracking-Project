c4ee7ce5d6e66dcaf061bc2d554b05ed1b877b15
shoaib73510 <shyuaib007@gmail.com>
2025-04-01 15:48:52 +0530

    Initial commit

--- frontend/ap/vite.config.js
+++ frontend/ap/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FITNESS TRACKING PROJECT/Frontend/ap/",
+  base: "/FITNESS TRACKING PROJECT/",
   plugins: [react()],
 })
